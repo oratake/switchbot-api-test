8ff666afab35d0eb83f319359a3a513c80884f3d
oratake <smith.shimomura@gmail.com>
2026-01-14 08:24:27 +0900

    feat: API用にRust(axum)を立てる

---
+++ backend/src/main.rs
@@ -0,0 +1,22 @@
+// 以下チュートリアル実施中
+// https://programatik29.github.io/axum-tutorial/hello_world.html
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // /hello-worldへのgetリクエストをhello_worldハンドラにパス
+    let app = Router::new().route("/hello-world",get(hello_world));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let listener = TcpListener::bind(addr).await?;
+
+    axum::serve(listener, app.into_make_service()).await?;
+
+    Ok(())
+}
+
+async fn hello_world() -> &'static str {
+    "Hello, World!"
+}
