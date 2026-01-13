b003c3aaa75fb8c341920accfa92f8324bf00241
oratake <smith.shimomura@gmail.com>
2026-01-13 17:32:38 +0900

    feat: function整理

--- test.js
+++ test.js
@@ -2,34 +2,53 @@ const crypto = require('crypto');
 const axios = require('axios');
 require('dotenv').config();
 
-const token = process.env.TOKEN;
-const secret = process.env.SECRET;
+const TOKEN = process.env.TOKEN;
+const SECRET = process.env.SECRET;
+const BASE_API_URL = 'https://api.switch-bot.com';
 
-async function getPlugMiniJP() {
+// 実行
+getDeviceData();
+
+function generateHeaders() {
     const t = Date.now();
-    const nonce = crypto.randomUUID(); // ランダムなUUIDを生成
-    const data = token + t + nonce;
+    const nonce = crypto.randomUUID();
+    const data = TOKEN + t + nonce;
     
     // HMAC-SHA256で署名を生成し、Base64エンコード後に大文字に変換
     const sign = crypto
-        .createHmac('sha256', secret)
+        .createHmac('sha256', SECRET)
         .update(Buffer.from(data, 'utf-8'))
         .digest('base64');
 
+    return {
+        'Authorization': TOKEN,
+        'sign': sign,
+        'nonce': nonce,
+        't': t,
+        'Content-Type': 'application/json',
+    };
+}
+
+async function requestAPI(api, method) {
+    const headers = generateHeaders();
+    const url = BASE_API_URL + api;
+
     const options = {
-        url: 'https://api.switch-bot.com/v1.1/devices',
-        method: 'GET',
-        headers: {
-            'Authorization': token,
-            'sign': sign,
-            'nonce': nonce,
-            't': t,
-            'Content-Type': 'application/json',
-        },
+        url: url,
+        method: method,
+        headers: headers,
     };
 
     try {
         const response = await axios(options);
+	return response;
+    } catch (error) {
+        console.error('APIの呼び出しに失敗しました:', error.response ? error.response.data : error.message);
+    }
+}
+
+async function getDeviceData() {
+	const response = await requestAPI('/v1.1/devices','GET');
         const { deviceList } = response.data.body;
 
         // deviceTypeが "Plug Mini (JP)" のものを抽出
@@ -39,10 +58,4 @@ async function getPlugMiniJP() {
 
         console.log('--- Plug Mini (JP) Devices ---');
         console.log(JSON.stringify(plugMiniJPDevices, null, 2));
-
-    } catch (error) {
-        console.error('APIの呼び出しに失敗しました:', error.response ? error.response.data : error.message);
-    }
 }
-
-getPlugMiniJP();
